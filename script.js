59e3a5d54d575ea8b6775791520a3a7c5be277c2
Fernando050 <140735183+Fernando050@users.noreply.github.com>
2026-05-12 22:03:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", ()=>{
+
+  const header = document.querySelector("header");
+
+  if(window.scrollY > 50){
+    header.style.background = "rgba(0,0,0,0.7)";
+  }else{
+    header.style.background = "rgba(0,0,0,0.3)";
+  }
+
+});
