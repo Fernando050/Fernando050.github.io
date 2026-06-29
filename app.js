5b99fe8b26db3b797da479e96d11abe32f107279
Fernando050 <140735183+Fernando050@users.noreply.github.com>
2026-06-29 14:54:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,124 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+getDatabase,
+ref,
+set,
+get,
+push,
+onChildAdded,
+remove
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+
+/* FIREBASE */
+const firebaseConfig = {
+  apiKey: "AIzaSyBo18YIHppmFTmwYpAueQKKwKP3GiGhwVU",
+  authDomain: "shadowchat-7fc89.firebaseapp.com",
+  databaseURL: "https://shadowchat-7fc89-default-rtdb.firebaseio.com",
+  projectId: "shadowchat-7fc89",
+  storageBucket: "shadowchat-7fc89.firebasestorage.app",
+  messagingSenderId: "597195787056",
+  appId: "1:597195787056:web:f8394aa1d27a76388a56f5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+let currentUser = "";
+let chatId = "";
+
+/* REGISTER */
+window.register = () => {
+  set(ref(db,"users/"+regId.value),{
+    id:regId.value,
+    password:regPass.value
+  });
+
+  alert("Account created ✔");
+};
+
+/* LOGIN */
+window.login = () => {
+
+  get(ref(db,"users/"+loginId.value)).then((snap)=>{
+
+    if(snap.exists() && snap.val().password === loginPass.value){
+
+      currentUser = loginId.value;
+
+      loginBox.classList.add("hidden");
+      chatBox.classList.remove("hidden");
+
+    } else {
+      alert("Wrong ID or password");
+    }
+  });
+};
+
+/* START CHAT */
+window.startChat = () => {
+
+  const other = targetUser.value;
+
+  chatId = [currentUser, other].sort().join("_");
+
+  messages.innerHTML = "";
+
+  loadMessages();
+};
+
+/* SEND MESSAGE */
+window.sendMessage = () => {
+
+  if(!msgInput.value) return;
+
+  push(ref(db,"chats/"+chatId+"/messages"),{
+    sender:currentUser,
+    text:msgInput.value,
+    time:Date.now()
+  });
+
+  msgInput.value="";
+};
+
+/* LOAD + DELETE */
+function loadMessages(){
+
+  const msgRef = ref(db,"chats/"+chatId+"/messages");
+
+  onChildAdded(msgRef, (data)=>{
+
+    const m = data.val();
+    const key = data.key;
+
+    const div = document.createElement("div");
+    div.classList.add("msg");
+
+    if(m.sender === currentUser){
+      div.classList.add("me");
+    }
+
+    div.innerHTML = `
+      <b>${m.sender}</b><br>${m.text}
+    `;
+
+    /* DELETE BUTTON ONLY FOR OWNER */
+    if(m.sender === currentUser){
+      const btn = document.createElement("button");
+      btn.innerText = "x";
+      btn.className = "deleteBtn";
+
+      btn.onclick = () => {
+        remove(ref(db,"chats/"+chatId+"/messages/"+key));
+        div.remove();
+      };
+
+      div.appendChild(btn);
+    }
+
+    messages.appendChild(div);
+    messages.scrollTop = messages.scrollHeight;
+  });
+}
+
+/* LOGOUT */
+window.logout = () => location.reload();
